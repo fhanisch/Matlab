a914e8bdbb0720e94b7e97e9b7b4001f764eb623
fhanisch <felix.hanisch@live.de>
2018-02-11 19:47:40 +0100

    update

--- perlin_sphere.m
+++ perlin_sphere.m
@@ -1,26 +1,25 @@
 clear;
 close all;
 
-u=0:181;
-v=0:361;
-g_x=2*rand(length(u),length(v))-1;
-g_y=2*rand(length(u),length(v))-1;
-x=0:0.5:180;
-y=0:0.5:360;
-X=zeros(length(x),length(y));
-Y=X;
-Z=X;
+x=0:0.005:1;
+y=0:0.005:1;
+[X,Y]=meshgrid(x,y);
 
 for j=1:length(y)
     for i=1:length(x)
-          
-        dR=0.5*perlin_interp2(0.2*x(i),0.2*y(j),g_x,g_y);
-        X(i,j)=(5-dR)*sin(x(i)*pi/180)*cos(y(j)*pi/180);
-        Y(i,j)=(5-dR)*sin(x(i)*pi/180)*sin(y(j)*pi/180);
-        Z(i,j)=(5-dR)*cos(x(i)*pi/180);
+        u=acos(cos(x(i)*2*pi)*sin(y(j)*pi));
+        v=asin(sin(y(j)*pi)*sin(x(i)*2*pi));
+        dR=0.25*perlin_interp2(4*u, 4*v);
+        X(i,j)=(1+dR)*sin(y(j)*pi)*cos(x(i)*2*pi);
+        Y(i,j)=(1+dR)*sin(y(j)*pi)*sin(x(i)*2*pi);
+        Z(i,j)=(1+dR)*cos(y(j)*pi);
     end
 end
 
-surfl(X,Y,Z);
+
+h=surfl(X,Y,Z);
 shading interp
-colormap(bone);
+hold on
+axis equal
+camlight
+set(h,'facecolor',[1 0 0]); 
