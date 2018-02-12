6c6fe070d5180d4798175e9acefab3aa8bcbf012
fhanisch <felix.hanisch@live.de>
2018-02-12 21:50:18 +0100

    update

--- perlin_sphere.m
+++ perlin_sphere.m
@@ -7,12 +7,13 @@ y=0:0.005:1;
 
 for j=1:length(y)
     for i=1:length(x)
-        u=acos(cos(x(i)*2*pi)*sin(y(j)*pi));
-        v=asin(sin(y(j)*pi)*sin(x(i)*2*pi));
-        dR=0.25*perlin_interp2(4*u, 4*v);
-        X(i,j)=(1+dR)*sin(y(j)*pi)*cos(x(i)*2*pi);
-        Y(i,j)=(1+dR)*sin(y(j)*pi)*sin(x(i)*2*pi);
-        Z(i,j)=(1+dR)*cos(y(j)*pi);
+        X(i,j)=sin(y(j)*pi)*cos(x(i)*2*pi);
+        Y(i,j)=sin(y(j)*pi)*sin(x(i)*2*pi);
+        Z(i,j)=cos(y(j)*pi);
+        dr = 0.1*perlin_interp2(4*X(i,j),4*Y(i,j)) + 0.1*perlin_interp2(4*X(i,j),4*Z(i,j));
+        X(i,j) = (1+dr)*X(i,j);
+        Y(i,j) = (1+dr)*Y(i,j);
+        Z(i,j) = (1+dr)*Z(i,j);
     end
 end
 
